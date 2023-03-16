c4ced6b2f871fad6c4c90acc84f6e97a7ba6e09a
dujulacis <59266348+Dujulacis@users.noreply.github.com>
2023-03-16 12:23:59 +0200

    Hotfix 2

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-#from faulthandler import disable
 import tkinter as tk
-#import os
-import getpass #iegust lietotajvardu
+import getpass  # iegust lietotajvardu
 from tkinter import BOTTOM, PhotoImage, messagebox, LEFT, RIGHT, TOP
 import json
 import random
 import requests
 
-open("flashcard.txt", "w").close() #iztira teksta dokumentu atverot programmu (nakotnes versijas to varetu izveleties lietotajs)
+# iztira teksta dokumentu atverot programmu (nakotnes versijas to varetu izveleties lietotajs)
+open("flashcard.txt", "w").close()
 
 
 # Window setup
@@ -29,13 +28,15 @@ c = 0
 removecount = 0
 
 # Functions
-def addCard(): #flashcard pievienosanas logs
+def addCard():  # flashcard pievienosanas logs
     nWa = tk.Toplevel()
     nWa.title("pyflash - create flashcards")
     nWa.geometry("960x540")
     nWa.configure(bg="#6985b3")
-    tk.Label(nWa, text="Guide: \n 1. Add your word \n 2. Add it's definition/meaning/translation \n 3. Create flashcard \n 4. Repeat with the next words  ", font=('Helvetica bold', 10), wraplength=440, justify="center", bg="white").pack(expand=True)
-    tk.Label(nWa, text="word", font=('Helvetica bold', 20), bg="white").pack(expand=True)
+    tk.Label(nWa, text="Guide: \n 1. Add your word \n 2. Add it's definition/meaning/translation \n 3. Create flashcard \n 4. Repeat with the next words  ",
+             font=('Helvetica bold', 10), wraplength=440, justify="center", bg="white").pack(expand=True)
+    tk.Label(nWa, text="word", font=('Helvetica bold', 20),
+             bg="white").pack(expand=True)
     tk.Entry(nWa, bd=1, font=('Helvetica bold', 20),
              textvariable=word, bg="white").pack(expand=True)
     tk.Label(nWa, text="meaning", font=(
@@ -49,12 +50,11 @@ def addCard(): #flashcard pievienosanas logs
     tk.Label(nWa, textvariable=count, font=(
         'Helvetica bold', 10), bg="white").pack()
     tk.Button(nWa, text="clear flashcards", font=(
-        'Helvetica bold', 10), command=clearcard, bg="white").pack(expand=True, side=BOTTOM)    
+        'Helvetica bold', 10), command=clearcard, bg="white").pack(expand=True, side=BOTTOM)
 
 
-
-def register(): #funkcija, kas parbauda vai ir ievaditas nepieciesamas vertibas kartinam,
-     #pieskaita pievienoto kartinu skaitu un pievieno tas teksta dokumenta json formata
+def register():  # funkcija, kas parbauda vai ir ievaditas nepieciesamas vertibas kartinam,
+    # pieskaita pievienoto kartinu skaitu un pievieno tas teksta dokumenta json formata
     global c
     if len(word.get()) == 0:
         messagebox.showerror("No word", "No word entry")
@@ -72,7 +72,8 @@ def register(): #funkcija, kas parbauda vai ir ievaditas nepieciesamas vertibas 
             )
             f.write("\n")
 
-def clearcard(): #nodrosina kartinu notirisanu
+
+def clearcard():  # nodrosina kartinu notirisanu
     global rndo, c, removecount, count
     open("flashcard.txt", "w").close()
     rndo = 0
@@ -80,17 +81,19 @@ def clearcard(): #nodrosina kartinu notirisanu
     removecount = 0
     count.set(0)
 
-def viewCard(): #kartinu apskates/macisanas logs
+
+def viewCard():  # kartinu apskates/macisanas logs
     if c < 1:
-        messagebox.showerror("No flashcard entry", "No flashcard entry") #parbauda vai ir izveidota vismaz viena kartina
+        # parbauda vai ir izveidota vismaz viena kartina
+        messagebox.showerror("No flashcard entry", "No flashcard entry")
     else:
         nWb = tk.Toplevel()
         nWb.title("pyflash - view flashcards")
         nWb.geometry("960x540")
         nWb.configure(bg="#6985b3")
-        removedlist=[] #saraksts ar lietotaja zinatajiem vardiem
+        removedlist = []  # saraksts ar lietotaja zinatajiem vardiem
 
-        def randomizer(): #funkcija, kas izvelas nejausi izveletu vardu un skaidrojumu
+        def randomizer():  # funkcija, kas izvelas nejausi izveletu vardu un skaidrojumu
             hintbutton.config(state=tk.NORMAL)
             hinttext.config(textvariable=0)
             global rndo
@@ -99,38 +102,40 @@ def viewCard(): #kartinu apskates/macisanas logs
             print(rndo)
             if rndo in removedlist:
                 if removecount == (count.get()):
-                    winmsg = messagebox.askquestion("You won!", "Try again?", icon="question") #kad lietotajs "uzvar", izvele spelet atkal vai ne
+                    # kad lietotajs "uzvar", izvele spelet atkal vai ne
+                    winmsg = messagebox.askquestion(
+                        "You won!", "Try again?", icon="question")
                     if winmsg == "yes":
-                        removecount=0
+                        removecount = 0
                         removedlist.clear()
                     else:
                         nWb.destroy()
-                        removecount=0
+                        removecount = 0
                         removedlist.clear()
                 else:
                     randomizer()
-            
+
             with open('flashcard.txt') as g:
-                readline = g.readlines()[rndo] #nolasa random liniju teksta dokumenta
+                # nolasa random liniju teksta dokumenta
+                readline = g.readlines()[rndo]
 
             result = json.loads(readline)
             wordg.set(result["Word"])
             defig.set(result["Definition"])
 
-
-        def hint(): #funkcija, kas parada atbildi
+        def hint():  # funkcija, kas parada atbildi
             hintbutton.config(state=tk.DISABLED)
             hinttext.config(textvariable=defig)
 
-
-        def remover(): #funkcija, kas iznem vardu (liniju) no random
+        def remover():  # funkcija, kas iznem vardu (liniju) no random
             global removecount
             hintbutton.config(state=tk.NORMAL)
             hinttext.config(textvariable=0)
             removedlist.append(rndo)
-            removecount+=1
+            removecount += 1
             randomizer()
-        tk.Label(nWb, text="Guide: \n 1. Guess the word \n 2. If you remember it, press know, to remove it from the list. \n 3. If you don't know the meaning, you can use hint to uncover it. \n 4. Press next for unknown words. \n 5. Repeat till all words are known. \n 6. Repeat the process until you are happy with your knowledge!",  font=('Helvetica bold', 10), wraplength=440, justify="center", bg="white").pack(expand=True)    
+        tk.Label(nWb, text="Guide: \n 1. Guess the word \n 2. If you remember it, press know, to remove it from the list. \n 3. If you don't know the meaning, you can use hint to uncover it. \n 4. Press next for unknown words. \n 5. Repeat till all words are known. \n 6. Repeat the process until you are happy with your knowledge!",  font=(
+            'Helvetica bold', 10), wraplength=440, justify="center", bg="white").pack(expand=True)
         tk.Label(nWb, textvariable=wordg, font=(
             'Helvetica bold', 20), bg="white").pack(expand=True)
         hinttext = tk.Label(nWb, textvariable=0, font=(
@@ -145,28 +150,28 @@ def viewCard(): #kartinu apskates/macisanas logs
         hintbutton = tk.Button(nWb, text="hint", font=(
             'Helvetica bold', 20), command=hint, state=tk.NORMAL, bg="white")
         hintbutton.pack(expand=True)
-        
-        
+
         randomizer()
 
 
-def options(): #opciju logs, kura lietotajs varetu mainit dazadas opcijas (nakotne)
+def options():  # opciju logs, kura lietotajs varetu mainit dazadas opcijas (nakotne)
     nWc = tk.Toplevel()
     nWc.title("pyflash - options")
     nWc.geometry("960x540")
     nWc.configure(bg="#6985b3")
 
 
-def get_random_quote(): #funkcija, kas izmantojot api iegust random quote
+def get_random_quote():  # funkcija, kas izmantojot api iegust random quote
     random_quote = requests.get("http://api.quotable.io/random").json()
     quote = random_quote["content"] + "\n\n" + "~" + random_quote["author"]
     return quote
-     
+
 
 # Main window code
 tk.Label(root, text="hey " + str(getpass.getuser()) +
          ", welcome to pyflash!", font=('Helvetica bold', 40), bg="white", wraplength=1100, justify="center").pack(expand=True)
-tk.Label(root, text=get_random_quote(), font=('Helvetica bold', 15, ), wraplength=1000, justify="center", bg="white").pack()
+tk.Label(root, text=get_random_quote(), font=('Helvetica bold', 15, ),
+         wraplength=1000, justify="center", bg="white").pack()
 tk.Button(root, text="1. \n create flashcards", width=20, height=3, font=(
     'Helvetica bold', 20), command=addCard, bg="white").pack(expand=True, side=LEFT)
 tk.Button(root, text="options", width=20, height=3, font=(
